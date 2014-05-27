0aabf8837d49ddf28874c2689203dec2d5878fc8
Dav Evans <dav@dav-evans.com>
2014-05-27 19:50:41 +1000

    Initial SQL commit

---
+++ Church/Church.SQL/Core/Tables/Church.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Core].[Church] (
+    [Id]		INT				NOT NULL, 
+	[Name]		VARCHAR(255)	NOT NULL,
+    CONSTRAINT [PK_Core.Church_Id] PRIMARY KEY CLUSTERED ([Id]),
+	CONSTRAINT [UC_Core.Church_Name] UNIQUE ([Name])
+);
+
