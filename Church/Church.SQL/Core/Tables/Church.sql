a5bbdb8eb1b175427954c5d27876afda3baad71f
Dav Evans <dav@dav-evans.com>
2014-06-29 21:06:17 +1000

    Remove Church.SQL

--- Church/Church.SQL/Core/Tables/Church.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [Core].[Church] (
-    [Id]		INT				NOT NULL, 
-	[Name]		VARCHAR(255)	NOT NULL,
-    CONSTRAINT [PK_Core.Church_Id] PRIMARY KEY CLUSTERED ([Id]),
-	CONSTRAINT [UC_Core.Church_Name] UNIQUE ([Name])
-);
-
